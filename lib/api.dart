b8c86b51b82fafe88094fda338fe2fa423e2571b
Justin42 <1357683+justin42@users.noreply.github.com>
2023-05-08 20:57:19 -0400

    Remove default implementation for DsBuildApi.verifyDescriptor

--- lib/api.dart
+++ lib/api.dart
@@ -11,41 +11,7 @@ abstract class DsBuildApi {
 
   DsBuildApi(this.config, this.repository, this.registry);
 
-  List<String> verifyDescriptor() {
-    List<String> errors = [];
-
-    // Verify preprocessors
-    for (StepDescriptor step in repository.descriptor.preprocessorSteps) {
-      if (!registry.preprocessors.containsKey(step.type)) {
-        errors.add("No preprocessor matching type '${step.type}'");
-      }
-    }
-
-    // Verify readers
-    for (InputDescriptor input in repository.descriptor.inputs) {
-      if (!registry.readers.containsKey(input.format)) {
-        errors.add("No reader matching type '${input.format}'");
-      }
-    }
-
-    // Verify postprocessors
-    for (OutputDescriptor output in repository.descriptor.outputs) {
-      for (StepDescriptor step in output.steps) {
-        if (!registry.postprocessors.containsKey(step.type)) {
-          errors.add("No postprocessor matching type '${output.format}");
-        }
-      }
-    }
-
-    // Verify writers
-    for (OutputDescriptor output in repository.descriptor.outputs) {
-      if (!registry.writers.containsKey(output.format)) {
-        errors.add("No writer matching type '${output.format}'");
-      }
-    }
-
-    return errors;
-  }
+  List<String> verifyDescriptor();
 
   Stream<InputDescriptor> fetchRequirements();
 }
