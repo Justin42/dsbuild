2a0cadf06fe9aad4f6e33d345031ca9b2c0d67bf
Justin42 <1357683+justin42@users.noreply.github.com>
2023-05-08 20:57:39 -0400

    Remove unused logger

--- lib/repository.dart
+++ lib/repository.dart
@@ -1,8 +1,4 @@
-import 'package:logging/logging.dart';
-
 import 'model/descriptor.dart';
-
-final Logger log = Logger("dsbuild/Repository");
 
 /// Responsible for storing data during the build process.
 class Repository {
