455a4cc0aa9b2bfee77db3bcf7047e7011127b9b
Justin42 <1357683+justin42@users.noreply.github.com>
2023-05-11 05:24:36 -0400

    DsBuildWriter, uses Conversation model

---
+++ lib/writer/dsbuild_writer.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dsbuild/model/conversation.dart';
+
+import 'writer.dart';
+
+class DsBuildWriter extends Writer {
+  const DsBuildWriter(super.config);
+
+  @override
+  Stream<Conversation> write(
+      Stream<Conversation> conversations, String destination) async* {
+    IOSink ioSink = await File(destination)
+        .create(recursive: true)
+        .then((value) => value.openWrite());
+    ioSink.writeln("[");
+
+    yield* conversations.map((event) {
+      ioSink.write(jsonEncode(event.toJson()));
+      ioSink.write(',\n');
+      return event;
+    });
+
+    ioSink.writeln("]");
+    ioSink.close();
+  }
+}
