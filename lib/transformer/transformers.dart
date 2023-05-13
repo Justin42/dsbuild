9285d53e4cde74ece07dd7e0658049478fed24e6
Justin42 <1357683+justin42@users.noreply.github.com>
2023-05-13 10:32:00 -0400

    Implement RegexExtract, partial implementation for Participants

--- lib/transformer/transformers.dart
+++ lib/transformer/transformers.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:collection';
+import 'dart:io';
 
 import 'package:dsbuild/transformer/postprocessor.dart';
 import 'package:html/parser.dart';
@@ -35,6 +36,8 @@ class Trim extends Preprocessor {
 }
 
 class RegexReplace extends Preprocessor {
+  final List<RegExp> regex;
+
   RegexReplace(super.config)
       : regex = [
           for (List r in config['replacements'])
@@ -44,7 +47,6 @@ class RegexReplace extends Preprocessor {
                 unicode: config['unicode'] ?? false,
                 dotAll: config['dotAll'] ?? false)
         ];
-  final List<RegExp> regex;
 
   @override
   String get description => "Regex pattern replacement";
@@ -60,10 +62,73 @@ class RegexReplace extends Preprocessor {
       });
 }
 
+class RegexExtract extends Preprocessor {
+  final List<RegExp> regex;
+  final File file;
+  IOSink? ioSink;
+  final bool escape;
+
+  RegexExtract(super.config)
+      : regex = [
+          for (String pattern in config['patterns'])
+            RegExp(pattern,
+                multiLine: config['multiLine'] ?? false,
+                caseSensitive: config['caseSensitive'] ?? true,
+                unicode: config['unicode'] ?? false,
+                dotAll: config['dotAll'] ?? false)
+        ],
+        file = File(config['path']),
+        //TODO: buffer = config['buffer'] ?? false,
+        escape = config['escape'];
+
+  @override
+  String get description => "Extract content matching pattern.";
+
+  @override
+  StreamTransformer<MessageEnvelope, MessageEnvelope> get transformer =>
+      StreamTransformer.fromHandlers(handleData: (data, sink) {
+        // convoId/username:
+        // match1
+        // match2
+        // ...
+        // \n
+        ioSink ??= file.openWrite(
+            mode: config['overwrite'] ? FileMode.writeOnly : FileMode.append);
+        bool matches = false;
+        for (RegExp pattern in regex) {
+          data.value.replaceAllMapped(pattern, (match) {
+            if (!matches) {
+              ioSink?.writeln("${data.conversationId}/${data.from}:");
+              matches = true;
+            }
+            if (escape) {
+              ioSink!.writeln(data.value
+                  .substring(match.start, match.end)
+                  .replaceAll("\n", r"\n"));
+            } else {
+              ioSink!.writeln(data.value.substring(match.start, match.end));
+            }
+            return match.input;
+          });
+        }
+        if (matches) {
+          ioSink!.writeln();
+        }
+        sink.add(data);
+      }, handleDone: (sink) async {
+        await ioSink?.flush();
+        await ioSink?.close();
+        sink.close();
+      });
+}
+
 class ExactReplace extends Preprocessor {
   final List replacements;
+  final bool recursive;
 
-  ExactReplace(super.config) : replacements = config['replacements'];
+  ExactReplace(super.config)
+      : replacements = config['replacements'],
+        recursive = config['recursive'] ?? false;
 
   @override
   String get description => "Simple substitution on exact match.";
@@ -71,11 +136,20 @@ class ExactReplace extends Preprocessor {
   @override
   StreamTransformer<MessageEnvelope, MessageEnvelope> get transformer =>
       StreamTransformer.fromHandlers(handleData: (data, sink) {
-        String newText = data.value;
-        for (List replacement in replacements) {
-          newText = newText.replaceAll(replacement[0], replacement[1]);
-        }
-        sink.add(data.copyWithValue(newText));
+        String lastText = data.value;
+        List<bool> hasMatches = List.filled(replacements.length, false);
+        do {
+          for (int i = 0; i < replacements.length; i++) {
+            hasMatches[i] = false;
+            String text =
+                lastText.replaceAll(replacements[i][0], replacements[i][1]);
+            if (text != lastText) {
+              lastText = text;
+              hasMatches[i] = true;
+            }
+          }
+        } while (hasMatches.contains(true) && recursive);
+        sink.add(data.copyWithValue(lastText));
       });
 }
 
@@ -89,37 +163,80 @@ class ExactMatch extends Preprocessor {
   ExactMatch(super.config)
       : patterns = [for (String patterns in config['patterns']) patterns],
         action = ExactMatchAction.values.byName(config['action']),
-        caseSensitive = config['caseSensitive'];
+        caseSensitive = config['caseSensitive'] ?? true;
 
   @override
   String get description {
     switch (action) {
       case ExactMatchAction.drop:
         return "Drop messages that exactly match the provided pattern.";
-        break;
     }
   }
 
   @override
   StreamTransformer<MessageEnvelope, MessageEnvelope> get transformer =>
       StreamTransformer.fromHandlers(handleData: (data, sink) {
+        bool skip = false;
         for (String pattern in patterns) {
           switch (action) {
             case ExactMatchAction.drop:
-              if (caseSensitive &&
+              if (!caseSensitive &&
                   (data.value.toLowerCase() == pattern.toLowerCase())) {
-                continue;
+                skip = true;
+                break;
               } else if (data.value == pattern) {
-                continue;
+                skip = true;
+                break;
               }
-              break;
           }
+          if (skip) break;
+        }
+        if (!skip) {
           sink.add(data);
-          break;
         }
       });
 }
 
+class Participants extends Postprocessor {
+  final int min;
+  final int? max;
+  final bool alternating;
+
+  Participants(super.config)
+      : min = config['min'] ?? 0,
+        max = config['max'],
+        alternating = config['alternating'];
+
+  @override
+  String get description =>
+      "Filter conversations according to participant count.";
+
+  @override
+  StreamTransformer<Conversation, Conversation> get transformer =>
+      StreamTransformer.fromHandlers(handleData: (data, sink) {
+        String? lastParticipant;
+        HashSet<String> participants = HashSet();
+        bool skip = false;
+        for (Message message in data.messages) {
+          if (lastParticipant == message.from && alternating) {
+            skip = true;
+            break;
+          }
+          participants.add(message.from);
+          lastParticipant ??= message.from;
+          if (max != null && participants.length > max!) {
+            skip = true;
+            break;
+          }
+        }
+        if (!skip && participants.length > min) {
+          print(participants.length);
+          sink.add(data);
+        }
+      });
+}
+
+/*
 class Punctuation extends Preprocessor {
   const Punctuation(super.config);
 
