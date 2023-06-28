bb2a2d7069e9d5625fdf9abcaff94f9a4be05418
Justin42 <1357683+justin42@users.noreply.github.com>
2023-06-27 23:31:55 -0400

    Optional separate output file for conversations and vocabulary

--- lib/src/transformers/builtin/stats/stats_output.dart
+++ lib/src/transformers/builtin/stats/stats_output.dart
@@ -13,10 +13,25 @@ class StatsOutput extends ConversationTransformer {
   /// Output file
   final File file;
 
+  /// Separate output for vocabulary
+  final File? vocabFile;
+
+  /// Separate output for conversations
+  final File? conversationsFile;
+
+  /// Json output indent for pretty printing
+  final String indent;
+
   /// Create new instance
   StatsOutput(super.config)
       : stats = Stats(),
-        file = File(config['path']);
+        file = File(config['path']),
+        vocabFile =
+            config['vocabPath'] != null ? File(config['vocabPath']) : null,
+        conversationsFile = config['conversationsPath'] != null
+            ? File(config['conversationsPath'])
+            : null,
+        indent = config['indent'] ?? ' ';
 
   @override
   Stream<List<Conversation>> bind(Stream<List<Conversation>> stream) async* {
@@ -24,10 +39,33 @@ class StatsOutput extends ConversationTransformer {
       stats.pushAll(conversations);
       yield conversations;
     }
-    await file.create(recursive: true);
-    IOSink output = file.openWrite();
-    output.write(jsonEncode(stats.toMap()));
-    await output.flush();
-    await output.close();
+
+    // Prepare data
+    Map<String, dynamic> result = stats.toMap();
+    Map<String, dynamic>? vocab =
+        vocabFile != null ? result.remove('vocabulary') : null;
+    Map<String, dynamic>? conversations =
+        conversationsFile != null ? result.remove('conversations') : null;
+
+    // Setup encoder
+    JsonEncoder encoder = JsonEncoder.withIndent(indent);
+
+    // Write output files
+    for ((File, Map<String, dynamic>) out in [
+      // Main
+      (file, result),
+      // Vocab
+      if (vocabFile != null) (vocabFile!, vocab ?? <String, dynamic>{}),
+      // Conversation
+      if (conversationsFile != null)
+        (conversationsFile!, conversations ?? <String, dynamic>{})
+    ]) {
+      var (File outputFile, outputData) = out;
+      await outputFile.create(recursive: true);
+      IOSink output = outputFile.openWrite();
+      output.write(encoder.convert(outputData));
+      await output.flush();
+      await output.close();
+    }
   }
 }
