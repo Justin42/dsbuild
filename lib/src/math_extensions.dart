e7e3451c155118415ae7e0101aecde5b28688720
Justin42 <1357683+justin42@users.noreply.github.com>
2023-06-28 07:07:40 -0400

    Allow standardDeviation to provide sample or population standard deviations

--- lib/src/math_extensions.dart
+++ lib/src/math_extensions.dart
@@ -5,7 +5,7 @@ import 'package:collection/collection.dart';
 /// Functions to sort a list of comparables and return a wrapped version.
 extension ToSortedList<E extends num> on Iterable<E> {
   /// Sort the list and wrap it in a [SortedList]
-  SortedList<E> toSortedList() => SortedList(toList(growable: false));
+  SortedList<E> toSortedList() => SortedList(this);
 }
 
 /// Collect sum from an iterable
@@ -18,9 +18,12 @@ extension Sum<E extends num> on Iterable<E> {
 class SortedList<E extends Comparable<num>> extends UnmodifiableListView<E> {
   /// Sorts the base list and returns this wrapper instance
   ///
-  /// This mutates the original list and assumes that it has not been previously sorted.
-  SortedList(super.base, {final int Function(E a, E b)? compare}) {
-    compare != null ? sort(compare) : sort();
+  /// This assumes the list has not been previously sorted.
+  /// For iterables which copy on [Iterable.toList] this will also copy.
+  SortedList(Iterable<E> source, {final int Function(E a, E b)? compare})
+      : super(source) {
+    List<E> data = source.toList();
+    compare != null ? data.sort(compare) : data.sort();
   }
 
   /// Wrap the base list and assume it has already been sorted.
@@ -39,13 +42,16 @@ extension SortedNumericList<E extends num> on SortedList<E> {
     }
   }
 
-  /// Standard variation
-  double standardDeviation(double mean) {
+  /// Standard deviation.
+  ///
+  /// If [population] is true returns population standard deviation `σ`
+  /// If [population] is false returns sample standard deviation `s`
+  double standardDeviation(double mean, [bool population = false]) {
     if (length == 1) return 0;
     double temp = 0;
     for (var e in this) {
-      temp += (e - mean) * (e - mean);
+      temp += pow(e - mean, 2);
     }
-    return sqrt(temp / (length - 1));
+    return population ? sqrt(temp / length) : sqrt(temp / (length - 1));
   }
 }
