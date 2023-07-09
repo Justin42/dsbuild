528a02e0348cb4a4ade51d4e2bc70d65160bd41c
Justin42 <1357683+justin42@users.noreply.github.com>
2023-07-09 14:54:33 -0400

    Fix FastChatInput

--- lib/src/transformers/builtin/input/fastchat_input.dart
+++ lib/src/transformers/builtin/input/fastchat_input.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:dsbuild/progress.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 
-import '../../../../progress.dart';
 import '../../../conversation.dart';
 import '../../transformers.dart';
 
@@ -13,9 +13,17 @@ class FastChatInput extends ConversationTransformer {
   /// Input file path
   final String path;
 
+  /// Whether to preserve conversation Id's
+  final bool preserveIds;
+
+  /// Conversation batch size
+  final int conversationBatch;
+
   /// Create new instance
   FastChatInput(super.config, {super.progress})
-      : path = config['path'].toString();
+      : path = config['path'].toString(),
+        preserveIds = config['preserveIds'] ?? false,
+        conversationBatch = config['conversationBatch'] ?? 100;
 
   @override
   // TODO: implement description
@@ -23,21 +31,39 @@ class FastChatInput extends ConversationTransformer {
 
   @override
   Stream<List<Conversation>> bind(Stream<List<Conversation>> stream) async* {
-    List<dynamic> json = jsonDecode(await File(path).readAsString());
+    yield* stream;
 
-    yield* Stream.fromIterable(json).transform(
-        StreamTransformer.fromHandlers(handleData: (conversation, sink) {
-      int nextId = 0;
-      sink.add([
-        Conversation(conversation['id'].hashCode,
-            messages: [
-              for (dynamic message in conversation['conversations'])
-                Message(nextId++, message['from']!, message['value']!)
-            ].lock,
-            meta: IMap({'inputId': conversation['id']}))
-      ]);
-      progress?.add(ConversationRead(count: 1));
-      progress?.add(MessageRead(count: conversation['conversations'].length));
-    }));
+    List<Conversation> conversations = [];
+    List<dynamic> json = jsonDecode(await File(path).readAsString());
+    int nextId = 0;
+    int messagesRead = 0;
+
+    for (Map<String, dynamic> data in json) {
+      IList<Message> messages = [
+        for (Map<String, dynamic> message in data['conversations'] ?? [])
+          Message(
+              (preserveIds && message['id'] != null) ? message['id'] : nextId++,
+              message['from'],
+              message['value'])
+      ].lockUnsafe;
+      Conversation conversation = Conversation(data['id'],
+          messages: messages, meta: data['meta'] ?? const IMapConst({}));
+      conversations.add(conversation);
+      messagesRead += messages.length;
+
+      // Yield batch
+      if (conversations.length >= conversationBatch) {
+        yield conversations;
+        progress?.add(ConversationRead(count: conversations.length));
+        progress?.add(MessageRead(count: messagesRead));
+        messagesRead = 0;
+        conversations = [];
+      }
+    }
+
+    // Yield remaining
+    yield conversations;
+    progress?.add(ConversationRead(count: conversations.length));
+    progress?.add(MessageRead(count: messagesRead));
   }
 }
